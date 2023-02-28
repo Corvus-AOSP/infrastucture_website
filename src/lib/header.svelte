06b09fe6e25031ae4b9756b79a71c4eca4e85105
Fosslover69 <no.1opensourcelover@protonmail.com>
2023-02-28 22:22:46 +0530

    corvus-site: Add Scroll Behaviour

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,16 +1,21 @@
+<script>
+	// @ts-nocheck
+	import { scrollto } from 'svelte-scrollto';
+</script>
+
 <main>
 	<div
-		class="flex lg:flex-row item-center rounded-lg flex-col gap-2 w-full fixed top-0 left-0 justify-between lg:px-24 lg:py-8 py-4 items-center"
+		class="flex lg:flex-row item-center rounded-lg flex-col gap-2 w-screen fixed top-0 left-0 justify-between lg:px-24 lg:py-8 py-4 items-center"
 		style="backdrop-filter: blur(10px); background-color: rgba(33, 33, 43, 0.5);"
 	>
-		<div class="flex gap-4 items-center">
+		<div use:scrollto={'top'} class="flex gap-4 items-center">
 			<img src="https://ik.imagekit.io/corvus/Corvus_Logo.svg" alt="Corvus Logo" width="50px" />
 			<h1 class="font-extrabold text-4xl cursor-pointer">Corvus-Aosp</h1>
 		</div>
 		<div class="flex justify-evenly font-semibold items-center gap-2 lg:gap-16">
-			<div class="header">Specials</div>
-			<div class="header">Screenshots</div>
-			<div class="header">Team</div>
+			<div use:scrollto={'#specials'} class="header">Specials</div>
+			<div use:scrollto={'#screenshots'} class="header">Screenshots</div>
+			<div use:scrollto={'#'} class="header">Team</div>
 			<button class="lg:px-4 lg:py-2 px-2 py-1 bg-primary rounded-lg text-sm lg:text-lg"
 				>What's New</button
 			>
