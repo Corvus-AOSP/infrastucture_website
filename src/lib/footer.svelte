50a52fb5fc21de18c17acf7ea0da15294c066d80
Fosslover69 <no.1opensourcelover@protonmail.com>
2023-03-03 19:54:17 +0530

    corvus-site: Footer: Update footer icons and links

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -1,12 +1,22 @@
-<main class="bg-light-black lg:py-8 lg:px-32 px-4 py-6 rounded-lg flex justify-between items-center">
+<main
+	class="bg-light-black lg:py-8 lg:px-32 px-4 py-6 rounded-lg flex justify-between items-center"
+>
 	<div class="flex lg:gap-4 gap-1 items-center">
 		<img class="w-8" src="https://ik.imagekit.io/corvus/Corvus_Logo.svg" alt="Corvus Logo" />
 		<span class="font-extrabold lg:text-2xl text-lg">Corvus-Aosp</span>
 	</div>
 	<div class="flex gap-2 ">
-        <img class="w-8" src="https://ik.imagekit.io/corvus/Vector.svg" alt="Corvus Logo" />
-        <img class="w-8" src="https://ik.imagekit.io/corvus/Gpay.svg" alt="Corvus Logo" />
-        <img class="w-7" src="https://ik.imagekit.io/corvus/Paypal.svg" alt="Corvus Logo" />
-        <img class="w-8" src="https://ik.imagekit.io/corvus/Github.svg" alt="Corvus Logo" />
-    </div>
+		<a href="https://github.com/Corvus-Aosp" target="_blank" rel="noreferrer"
+			><img class="w-8" src="https://ik.imagekit.io/corvus/Github.svg" alt="Corvus Logo" /></a
+		>
+		<a href="upi://pay?pa=<riteshm321@oksbi" target="_blank"
+			><img class="w-8" src="https://ik.imagekit.io/corvus/Gpay.svg" alt="Corvus Logo" /></a
+		>
+		<a href="https://paypal.me/ritzz97" target="_blank" rel="noreferrer"
+			><img class="w-7" src="https://ik.imagekit.io/corvus/Paypal.svg" alt="Corvus Logo" /></a
+		>
+		<a href="https://telegram.dog/CorvusChannel" target="_blank" rel="noreferrer"
+			><img class="w-8" src="https://ik.imagekit.io/corvus/Telegram.svg" alt="Corvus Logo" /></a
+		>
+	</div>
 </main>
