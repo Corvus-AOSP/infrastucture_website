789a03acf4afe7321e145a5e8a97cc771aa61527
Fosslover69 <no.1opensourcelover@protonmail.com>
2023-03-03 19:57:38 +0530

    corvus-site: Navbar: Add Navbar Menu for Mobile

---
+++ src/lib/navbar.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	function toggleMenu() {
+		dispatch('toggleMenu', {});
+	}
+</script>
+
+<main>
+	<div
+		class="block lg:hidden fixed top-0 z-10 flex justify-center items-center w-screen h-screen backdrop-blur-md bg-light-black"
+	>
+		<div class="p-2 rounded-lg flex flex-col gap-6 items-center mx-2 text-2xl font-bold">
+			<button on:click={() => toggleMenu()}><i class="fas fa-times text-4xl" /></button>
+			<a href={'/#specials'}><button on:click={() => toggleMenu()}>Specials</button></a>
+			<a href={'/#screenshots'}><button on:click={() => toggleMenu()}>Screenshots</button></a>
+			<a href={'/#'}><button on:click={() => toggleMenu()}>Team</button></a>
+			<button>What's New</button>
+		</div>
+	</div>
+</main>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,21 @@
 <script>
 	import Footer from '$lib/footer.svelte';
 	import Header from '$lib/header.svelte';
+	import Navbar from '$lib/navbar.svelte';
 	import '../app.css';
+	let showMenu = false;
+	function toggleMenu() {
+		showMenu = !showMenu;
+	}
 </script>
 
 <div class="text-white">
 	<div class="absolute z-10">
-		<Header />
+		<Header on:toggleMenu={toggleMenu} />
 	</div>
+	{#if showMenu}
+		<Navbar on:toggleMenu={toggleMenu} />
+	{/if}
 	<slot />
 	<Footer />
 </div>
