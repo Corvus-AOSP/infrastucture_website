6ba7cbd178ff763e77b9ed5c299b93419c8495a0
Fosslover69 <no.1opensourcelover@protonmail.com>
2023-03-03 22:06:34 +0530

    corvus-site: Make site more responsive

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -1,12 +1,12 @@
-<main class="bg-light-black py-8 px-32 rounded-lg flex justify-between items-center">
-	<div class="flex gap-4 items-center">
-		<img src="https://ik.imagekit.io/corvus/Corvus_Logo.svg" alt="Corvus Logo" width="50px" />
-		<span class="font-extrabold text-2xl">Corvus-Aosp</span>
+<main class="bg-light-black lg:py-8 lg:px-32 px-4 py-6 rounded-lg flex justify-between items-center">
+	<div class="flex lg:gap-4 gap-1 items-center">
+		<img class="w-8" src="https://ik.imagekit.io/corvus/Corvus_Logo.svg" alt="Corvus Logo" />
+		<span class="font-extrabold lg:text-2xl text-lg">Corvus-Aosp</span>
 	</div>
-	<div class="flex gap-2">
-        <img src="https://ik.imagekit.io/corvus/Vector.svg" width="50px" alt="Corvus Logo" />
-        <img src="https://ik.imagekit.io/corvus/Gpay.svg" width="50px" alt="Corvus Logo" />
-        <img src="https://ik.imagekit.io/corvus/Paypal.svg" width="43px" alt="Corvus Logo" />
-        <img src="https://ik.imagekit.io/corvus/Github.svg" width="50px" alt="Corvus Logo" />
+	<div class="flex gap-2 ">
+        <img class="w-8" src="https://ik.imagekit.io/corvus/Vector.svg" alt="Corvus Logo" />
+        <img class="w-8" src="https://ik.imagekit.io/corvus/Gpay.svg" alt="Corvus Logo" />
+        <img class="w-7" src="https://ik.imagekit.io/corvus/Paypal.svg" alt="Corvus Logo" />
+        <img class="w-8" src="https://ik.imagekit.io/corvus/Github.svg" alt="Corvus Logo" />
     </div>
 </main>
--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,15 +1,15 @@
 <main>
 	<div
-		class="flex lg:flex-row item-center rounded-lg flex-col gap-2 w-screen fixed top-0 left-0 justify-between lg:px-24 lg:py-8 py-4 items-center"
+		class="flex lg:flex-row item-center rounded-lg flex-col gap-2 w-full fixed top-0 left-0 justify-between lg:px-24 lg:py-8 py-4 items-center"
 		style="backdrop-filter: blur(10px); background-color: rgba(33, 33, 43, 0.5);"
 	>
 		<div class="flex gap-4 items-center">
-            <img src="https://ik.imagekit.io/corvus/Corvus_Logo.svg" alt="Corvus Logo" width="50px">
-            <span class="font-extrabold text-4xl cursor-pointer">Corvus-Aosp</span></div>
+			<img src="https://ik.imagekit.io/corvus/Corvus_Logo.svg" alt="Corvus Logo" width="50px" />
+			<h1 class="font-extrabold text-4xl cursor-pointer">Corvus-Aosp</h1>
+		</div>
 		<div class="flex justify-evenly font-semibold items-center gap-2 lg:gap-16">
-			<div class="header">Home</div>
 			<div class="header">Specials</div>
-            <div class="header">Screenshots</div>
+			<div class="header">Screenshots</div>
 			<div class="header">Team</div>
 			<button class="lg:px-4 lg:py-2 px-2 py-1 bg-primary rounded-lg text-sm lg:text-lg"
 				>What's New</button
@@ -20,13 +20,13 @@
 
 <style>
 	.header {
-		color: #F4F0FF;
+		color: #f4f0ff;
 		cursor: pointer;
 		padding: 0.25rem 0.25rem;
 		border-radius: 0.5rem;
 	}
 	.header:hover {
-		color: #7D6DF8;
+		color: #7d6df8;
 		cursor: pointer;
 		background-color: #7e7efc41;
 		padding: 0.25rem 0.25rem;
