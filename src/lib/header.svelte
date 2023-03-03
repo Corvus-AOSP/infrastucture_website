829d2446ba524e0f1ea867a181b650900b62ad5b
Fosslover69 <no.1opensourcelover@protonmail.com>
2023-03-03 21:07:12 +0530

    corvus-site: Rearrange header items

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -35,8 +35,8 @@
 		</a>
 		<div class="lg:flex hidden justify-evenly font-semibold items-center gap-16">
 			<a href={'/#specials'} class="header">Specials</a>
-			<a href={'/#screenshots'} class="header">Screenshots</a>
 			<a href={'/#team'} class="header">Team</a>
+			<a href={'/#screenshots'} class="header">Screenshots</a>
 			<button class="lg:px-4 lg:py-2 px-2 py-1 bg-primary rounded-lg text-sm lg:text-lg"
 				>What's New</button
 			>
